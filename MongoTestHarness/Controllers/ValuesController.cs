1825ff6377da89ebf7a29d058b5f011743174ab9
karan9 <karan.srivastava@protonmail.com>
2021-05-19 00:59:23 +0530

    Initial check-in of module MongoTestHarness

---
+++ MongoTestHarness/Controllers/ValuesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using MongoTestHarness.Services;
+
+namespace MongoTestHarness.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+
+        private readonly MainService service;
+
+
+        public ValuesController(MainService s)
+        {
+            service = s;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        public static void startThread()
+        {
+            MainService main = new MainService(Thread.CurrentThread.Name);
+            Console.WriteLine("Thread Started ", Thread.CurrentThread.Name);
+            main.Unleash();
+        }
+
+
+        public static void startIngestion()
+        {
+            MainService main = new MainService(Thread.CurrentThread.Name);
+            main.CreateData();
+        }
+
+        public static void startQuerying()
+        {
+            MainService main = new MainService(Thread.CurrentThread.Name);
+            main.findRandomData();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post()
+        {
+            try
+            {
+                ThreadStart ingestionRef = new ThreadStart(startIngestion);
+                ThreadStart ingestionRef2 = new ThreadStart(startIngestion);
+                ThreadStart ingestionRef3 = new ThreadStart(startIngestion);
+                ThreadStart queryRef = new ThreadStart(startQuerying);
+                ThreadStart queryRef2 = new ThreadStart(startQuerying);
+                ThreadStart queryRef3 = new ThreadStart(startQuerying);
+
+                Thread ingestionThread = new Thread(ingestionRef);
+                Thread ingestionThread2 = new Thread(ingestionRef2);
+                Thread ingestionThread3 = new Thread(ingestionRef3);
+
+                Thread queryThread = new Thread(queryRef);
+                Thread queryThread2 = new Thread(queryRef2);
+                Thread queryThread3 = new Thread(queryRef3);
+
+                ingestionThread.Name = "Ingestion Thread";
+                ingestionThread2.Name = "Ingestion Thread 2";
+                ingestionThread3.Name = "Ingestion Thread 3";
+
+                queryThread.Name = "Query Thread";
+                queryThread2.Name = "Query Thread 2";
+                queryThread3.Name = "Query Thread 3";
+
+                ingestionThread.Start();
+                ingestionThread2.Start();
+                ingestionThread3.Start();
+
+                queryThread.Start();
+                queryThread2.Start();
+                queryThread3.Start();
+            } catch (Exception e)
+            {
+                Console.Write(e);
+            } finally
+            {
+                Console.Write("Finally Reached");
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MongoTestHarness/Services/MainService.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace MongoTestHarness.Services
+{
+    public class MainService
+    {
+        private readonly IMongoCollection<BsonDocument> baseCollection;
+        private readonly IMongoCollection<BsonDocument> airbnbCollection;
+        private readonly IMongoCollection<BsonDocument> moviesCollection;
+
+//        private readonly string CONNECTION_STRING = "mongodb+srv://ubisoft:ubisoft@cluster1.afhfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&minPoolSize=20&maxPoolSize=1000&waitQueueTimeoutMS=60000&maxIdleTimeMS=1500000";
+        private readonly string CONNECTION_STRING = "mongodb+srv://ubisoft:ubisoft@cluster1.afhfm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        private readonly string DB_NAME = "ubisoft";
+        private readonly string DB_AIRBNB = "sample_airbnb";
+        private readonly string DB_MOVIES = "sample_mflix";
+
+        private readonly string threadName;
+
+        public MainService()
+        {
+
+            var client = new MongoClient(CONNECTION_STRING);
+
+            Console.WriteLine(" =================  Verify Connection Settings ================");
+            Console.WriteLine("Client Max Pool Size " + client.Settings.MaxConnectionPoolSize.ToString());
+            Console.WriteLine("Client Min Pool Size " + client.Settings.MinConnectionPoolSize.ToString());
+            Console.WriteLine("Client MaxIdleTimeMS " + client.Settings.MaxConnectionIdleTime.ToString());
+            Console.WriteLine("Client WaitQueueTimeOutMS " + client.Settings.WaitQueueTimeout.ToString());
+            Console.WriteLine("Client Wait Queue Size" + client.Settings.WaitQueueSize.ToString());
+            Console.WriteLine(" =================  Verify Connection Settings ================");
+
+            // Mark databases
+            var databaseUbisoft = client.GetDatabase(DB_NAME);
+            var databaseSampleAirBNB = client.GetDatabase(DB_AIRBNB);
+            var databaseSampleMFLIX = client.GetDatabase(DB_MOVIES);
+
+
+            // Gather all the collections
+            baseCollection = databaseUbisoft.GetCollection<BsonDocument>("testdata");
+            airbnbCollection = databaseSampleAirBNB.GetCollection<BsonDocument>("listingsAndReviews");
+            moviesCollection = databaseSampleMFLIX.GetCollection<BsonDocument>("movies");
+        }
+
+        public MainService(string tname)
+        {
+
+            var client = new MongoClient(CONNECTION_STRING);
+
+            Console.WriteLine(" =================  Verify Connection Settings ================");
+            Console.WriteLine("Client Max Pool Size " + client.Settings.MaxConnectionPoolSize.ToString());
+            Console.WriteLine("Client Min Pool Size " + client.Settings.MinConnectionPoolSize.ToString());
+            Console.WriteLine("Client MaxIdleTimeMS " + client.Settings.MaxConnectionIdleTime.ToString());
+            Console.WriteLine("Client WaitQueueTimeOutMS " + client.Settings.WaitQueueTimeout.ToString());
+            Console.WriteLine("Client Wait Queue Size" + client.Settings.WaitQueueSize.ToString());
+            Console.WriteLine(" =================  Verify Connection Settings ================");
+
+            // Mark databases
+            var databaseUbisoft = client.GetDatabase(DB_NAME);
+            var databaseSampleAirBNB = client.GetDatabase(DB_AIRBNB);
+            var databaseSampleMFLIX = client.GetDatabase(DB_MOVIES);
+
+
+            // Gather all the collections
+            baseCollection = databaseUbisoft.GetCollection<BsonDocument>("testdata");
+            airbnbCollection = databaseSampleAirBNB.GetCollection<BsonDocument>("listingsAndReviews");
+            moviesCollection = databaseSampleMFLIX.GetCollection<BsonDocument>("movies");
+
+            threadName = tname;
+        }
+
+        public void findRandomData()
+        {
+            // Search 1 00 000 Times
+            for(var i=0; i < 100000; i++)
+            {
+                var filter = new BsonDocument
+                {
+                    { "room_type", "Entire home/apt" }
+                };
+
+                var data = airbnbCollection.Find<BsonDocument>(filter).ToList();
+                Console.WriteLine("Found " + data.Count + " Documents in the randomData on thread " + threadName);
+            }
+        }
+
+        public void aggregateRandomDataForTests()
+        {
+            //var data = moviesCollection.Aggregate<BsonDocument>(    );
+        }
+
+        public void CreateData()
+        {
+            // Run the Inserts 100 times
+            for(var counter = 0; counter < 100; counter++)
+            {
+                List<BsonDocument> inserts = new List<BsonDocument>();
+
+                // insert it way too many times;
+                for (var i = 0; i < 100000; i++)
+                {
+                    var document = new BsonDocument
+                    {
+                        { "student_id", i },
+                        { "scores", new BsonArray
+                            {
+                                new BsonDocument{ {"type", "exam"}, {"score", 88.12334193287023 } },
+                                new BsonDocument{ {"type", "quiz"}, {"score", 74.92381029342834 } },
+                                new BsonDocument{ {"type", "homework"}, {"score", 89.97929384290324 } },
+                                new BsonDocument{ {"type", "homework"}, {"score", 82.12931030513218 } }
+                            }
+                        },
+                        { "class_id", 480},
+                        { "_id", ObjectId.GenerateNewId() }
+                    };
+
+                    inserts.Add(document);
+                }
+
+
+                baseCollection.InsertMany(inserts);
+
+                // Log Current Iteration
+                Console.WriteLine("Current Iteration " + counter + " Inserted 100000 Documents from thread " + threadName);
+            }            
+        }
+
+
+        public void Unleash()
+        {
+            CreateData();
+            findRandomData();
+        }
+    }
+
+   
+}
+
+
+
+
+
+
